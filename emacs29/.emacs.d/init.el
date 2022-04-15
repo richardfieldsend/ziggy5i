f758977d0e39ccdbc27233f6d3273e60b1980b00
Richard Fieldsend <richardfieldsend@gmail.com>
2022-04-15 12:31:48 +0100

    Add emacs29 folder and init.el/config.org starting files. WIP

---
+++ emacs29/.emacs.d/init.el
@@ -0,0 +1,17 @@
+;; init.el - where Emacs customisation begins
+;;
+;; * Link to the config.org file which is a literate programming version of the standard Emacs config file.
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
