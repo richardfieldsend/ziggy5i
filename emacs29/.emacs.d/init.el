ece67c45ad2d41c1018c979ff033e135d66b76fa
Richard Fieldsend <richardfieldsend@gmail.com>
2022-05-05 00:06:42 +0100

    Mod init.el provide Org Babel support for more languages.

--- emacs29/.emacs.d/init.el
+++ emacs29/.emacs.d/init.el
@@ -8,6 +8,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages
+   '((emacs-lisp . t)
+     (awk . t)
+     (C . t)
+     (python . t)
+     (gnuplot . t)))
  '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
