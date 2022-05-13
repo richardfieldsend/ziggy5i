5df6002a0273125aa6c18fce1926f4725299b494
Richard Fieldsend <richardfieldsend@gmail.com>
2022-05-13 14:30:31 +0100

    Mod init.el file as part of configuring Povray mode and eBib.
    Povray is a raytracing program, and Emacs has a mode for editing the
    files required. The Povray mode includes triggers for running the
    render with high and highest image quality settings, though what is
    considered high quality reflects the fact that the available power was
    much lower than it is now. I have modified the high/highest image
    renders to both create images of the screen of my laptop (1920x1080).
    
    The ebib configuration is just an entry for where the keywords file is
    placed.

--- emacs29/.emacs.d/init.el
+++ emacs29/.emacs.d/init.el
@@ -8,13 +8,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ebib-keywords "~/bibliographies/keywordslist.txt")
  '(org-babel-load-languages
    '((emacs-lisp . t)
      (awk . t)
      (C . t)
      (python . t)
      (gnuplot . t)))
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages '(use-package))
+ '(pov-run-high "-H1080 -W1920 +i%s")
+ '(pov-run-highest "-H1080 -W1920 +i%s"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
